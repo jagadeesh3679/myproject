192bcd1a9daa578c25dd1ca6a63ffae90d008fd6
jadatravu <surya.janardhan@gmail.com>
2016-11-01 19:55:31 -0400

    New hellow view added

--- myp/mya/views.py
+++ myp/mya/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-# Create your views here.
 
 def hello(request):
     #return HttpResponse("hello world")
     return render_to_response("index.html")
 
 def hw(request):
+    #return HttpResponse("hello world")
+    return render_to_response("index.html")
+
+def hellow(request):
     #return HttpResponse("hello world")
     return render_to_response("index.html")
